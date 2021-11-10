05d5339fe6178bfb6589c8daaaa83454c62bba2b
Sergey <loveykosergey@ukr.net>
2021-11-10 16:59:55 +0200

    feat: task №3 - Training JS #1: create your first JS function and print 'Helloworld!'

---
+++ task_3.js
@@ -0,0 +1,9 @@
+// 3. - Training JS #1: create your first JS function and print "Helloworld!"
+
+//refer to the example and write your first JS function
+function helloWorld() {
+  var str = "Hello World!";
+  console.log(str);
+}
+
+helloWorld();
