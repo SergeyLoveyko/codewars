33c0cfcbe111a76e15ca292e615260db6d9f2581
Sergey <loveykosergey@ukr.net>
2021-11-12 17:09:19 +0200

    feat: task №11 - Keep up the hoop

---
+++ task_11.js
@@ -0,0 +1,13 @@
+// 11. - Keep up the hoop
+
+function hoopCount (n) {
+   //your code goes here
+    if (n >= 10) {
+        return "Great, now move on to tricks";
+    } else {
+        return "Keep at it until you get it";
+   }
+}
+
+console.log(hoopCount(3));      //  Keep at it until you get it
+console.log(hoopCount(11));     //  Great, now move on to tricks
