3a21fa4453f7dc3597d85c8919b1ccbb9d57b1fe
Sergey <loveykosergey@ukr.net>
2021-11-13 00:08:28 +0200

    refactor: task №17 - Opposite number

---
+++ task_17.js
@@ -0,0 +1,10 @@
+//  17. - Opposite number
+
+function opposite(number) {
+  //your code here
+  return number * -1;
+}
+
+console.log(opposite(1));       //   -1
+console.log(opposite(43));      //  -43
+console.log(opposite(-4));      //    4
