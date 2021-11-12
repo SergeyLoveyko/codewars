b1d714e6096d6c5b797e4562f31503e9ef298245
Sergey <loveykosergey@ukr.net>
2021-11-13 00:01:55 +0200

    refactor: task №16 - Opposites Attract

---
+++ task_16.js
@@ -0,0 +1,11 @@
+//  16. - Opposites Attract
+
+function lovefunc(flower1, flower2){
+  // moment of truth
+  return flower1 != flower2 ? true : false;
+}
+
+console.log(lovefunc(1,4));     //  true
+console.log(lovefunc(2,2));     //  false
+console.log(lovefunc(0,1));     //  true
+console.log(lovefunc(0,0));     //  false
