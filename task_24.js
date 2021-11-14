0e3311dc905b663ec9cced57fffc92b06f6da7ab
Sergey <loveykosergey@ukr.net>
2021-11-14 22:36:11 +0200

    feat: task №24 - Is it even?

---
+++ task_24.js
@@ -0,0 +1,12 @@
+//  24. - Is it even?
+
+function testEven(n) {
+    //Your awesome code here!
+    return (n % 2 === 0) ? true : false;
+}
+
+console.log(testEven(0));       //  true
+console.log(testEven(0.5));     //  false
+console.log(testEven(1));       //  false
+console.log(testEven(2));       //  true
+console.log(testEven(-4));      //  true
