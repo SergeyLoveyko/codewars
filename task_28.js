880217adf457913ea9b32d9c584a44203c1c5ee7
Sergey <loveykosergey@ukr.net>
2021-11-14 23:17:32 +0200

    feat: task №28 - Function 2 - squaring an argument

---
+++ task_28.js
@@ -0,0 +1,9 @@
+//  28. - Function 2 - squaring an argument
+
+function square(num) {
+    return Math.pow(num, 2);
+}
+
+console.log(square(2));     //   4
+console.log(square(3));     //   9
+console.log(square(7));     //  49
