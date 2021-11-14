893a5ff737d0df145ae3728096275b798d3f98f7
Sergey <loveykosergey@ukr.net>
2021-11-14 23:05:36 +0200

    feat: task №27 - Function 3 - multiplying two numbers

---
+++ task_27.js
@@ -0,0 +1,9 @@
+//  27. - Function 3 - multiplying two numbers
+
+function multiply(a, b) {
+    return a * b
+}
+
+console.log(multiply(3, 3));        //   9
+console.log(multiply(2, 7));        //  14
+console.log(multiply(10, 5));       //  50
