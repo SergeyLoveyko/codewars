91dcc9d152bedc25e71de6885afb7c618d2d82ba
Sergey <loveykosergey@ukr.net>
2021-11-16 20:07:54 +0200

    feat: task №36 - Return Negative

---
+++ task_36.js
@@ -0,0 +1,10 @@
+//  36. - Return Negative
+
+function makeNegative(num) {
+    // Code?
+    return num <= 0 ? num : num * -1;
+}
+
+console.log(makeNegative(42));      //   -42
+console.log(makeNegative(0));       //     0
+console.log(makeNegative(-14));      //  -14
