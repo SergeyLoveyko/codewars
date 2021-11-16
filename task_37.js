e254f36c7de8c407fa86f3bbf9bcd40bbd65bbfc
Sergey <loveykosergey@ukr.net>
2021-11-16 20:14:41 +0200

    feat: task №37 - Grasshopper - Variable Assignment Debug

---
+++ task_37.js
@@ -0,0 +1,10 @@
+//  37. - Grasshopper - Variable Assignment Debug
+
+var a = "dev";
+var b = "Lab";
+
+var name = a + b;
+
+console.log(a);         //  "dev"   
+console.log(b);         //  "Lab"
+console.log(name);      //  "devLab"
