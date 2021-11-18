6ad64922e7daed8b550c3d643dfe56b43643a70e
Sergey <loveykosergey@ukr.net>
2021-11-18 21:31:05 +0200

    feat: task №46 - Enumerable Magic #1 - True for All?

---
+++ task_46.js
@@ -0,0 +1,9 @@
+//  46. - Enumerable Magic #1 - True for All?
+
+function all( arr, fun ){
+    return arr.every(fun);
+}
+
+
+console.log(all([1,2,3,4,5], function(v){return v<9}));
+console.log(all([1,2,3,4,5], function(v){return v>9}));
