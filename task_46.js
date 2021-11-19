3e24f448783d6822fd1782b1f94479f08598785e
Sergey <loveykosergey@ukr.net>
2021-11-19 23:42:05 +0200

    feat: task №47 - Beginner Series #2 Clock

--- task_46.js
+++ task_46.js
@@ -5,5 +5,5 @@ function all( arr, fun ){
 }
 
 
-console.log(all([1,2,3,4,5], function(v){return v<9}));
-console.log(all([1,2,3,4,5], function(v){return v>9}));
+console.log(all([1,2,3,4,5], function(v){return v<9}));     //  true
+console.log(all([1,2,3,4,5], function(v){return v>9}));     //  false
