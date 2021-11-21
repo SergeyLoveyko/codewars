34abb22b1544df705114fc3c60c426af3e8afb9f
Sergey <loveykosergey@ukr.net>
2021-11-22 01:31:53 +0200

    feat: task №54 - Sum The Strings

---
+++ task_54.js
@@ -0,0 +1,9 @@
+//  54. - Sum The Strings
+
+function sumStr(a, b) {
+    const res = Number(a) + Number(b);
+    return res.toString();
+}
+
+console.log(sumStr("4", "5"));       //   "9"
+console.log(sumStr("34", "5"));      //  "39"
