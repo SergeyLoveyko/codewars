0cb3f42bf64fb32a49e11cca404762bb34292578
Sergey <loveykosergey@ukr.net>
2021-11-24 20:59:18 +0200

    feat: task №60 (7kyu) - Is this a triangle?

---
+++ task_60.js
@@ -0,0 +1,8 @@
+//  60. - Is this a triangle?
+
+function isTriangle(a,b,c) {
+   return a + b > c && a + c > b && b + c > a ? true : false;
+}
+
+console.log(isTriangle(1,2,2));        //  true
+console.log(isTriangle(7,2,2));        //  false
