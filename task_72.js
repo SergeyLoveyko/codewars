220ba4757d530a5e04e1a46e7bc59d495853795c
Sergey <loveykosergey@ukr.net>
2021-12-02 23:50:51 +0200

    feat: task №72 - Grasshopper - Function syntax debugging

---
+++ task_72.js
@@ -0,0 +1,8 @@
+//  72. - Grasshopper - Function syntax debugging
+
+function main(verb, noun) { 
+    return verb + noun;
+}
+
+console.log(main('take ', 'item'));      //  'take item'
+console.log(main('use ', 'sword'));      //  'use sword'
