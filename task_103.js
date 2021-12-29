8f6bd19f253f360ab03526dbb583028f5b140a91
Sergey <loveykosergey@ukr.net>
2021-12-29 22:18:44 +0200

    feat: task №103 - Convert a String to a Number!

---
+++ task_103.js
@@ -0,0 +1,10 @@
+//  103. - Convert a String to a Number!
+
+var stringToNumber = function(str){
+    return parseInt(str);
+}
+
+console.log(stringToNumber("1234"));     //  1234
+console.log(stringToNumber("605"));      //  605
+console.log(stringToNumber("1405"));     //  1405
+console.log(stringToNumber("-7"));       //  -7
