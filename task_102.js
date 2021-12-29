68acdbfc8dac3019afd2cbe939261fa4010f1492
Sergey <loveykosergey@ukr.net>
2021-12-29 22:10:24 +0200

    feat: task №102 - Convert a Number to a String!

---
+++ task_102.js
@@ -0,0 +1,9 @@
+//  102. - Convert a Number to a String!
+
+function numberToString(num) {
+    
+}
+
+console.log(numberToString(67));        //  '67'
+console.log(numberToString(627));       //  '627'
+console.log(numberToString(7));         //  '7'
