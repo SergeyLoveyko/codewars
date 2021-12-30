702d6ca3017f5b7b2a9509dc5607c7b406c09e92
Sergey <loveykosergey@ukr.net>
2021-12-30 23:22:23 +0200

    feat: task №104 - Convert to Binary

---
+++ task_104.js
@@ -0,0 +1,10 @@
+//  104. - Convert to Binary
+
+function toBinary(n){
+    return +n.toString(2);
+}
+
+console.log(toBinary(1));       //  1
+console.log(toBinary(2));       //  10
+console.log(toBinary(3));       //  11
+console.log(toBinary(5));       //  101 
