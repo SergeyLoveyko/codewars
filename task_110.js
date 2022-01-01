fcd2dd785914d14a5c2d7048d02c2990a5f8f8f0
Sergey <loveykosergey@ukr.net>
2022-01-01 23:23:52 +0200

    feat: task №110 - Largest Square Inside A Circle

---
+++ task_110.js
@@ -0,0 +1,9 @@
+//  110. - Largest Square Inside A Circle
+
+function areaLargestSquare(r) {
+    return 2 * (r * r); 
+}
+
+console.log(areaLargestSquare(5));          //      50
+console.log(areaLargestSquare(7));          //      98
+console.log(areaLargestSquare(15));         //      450
