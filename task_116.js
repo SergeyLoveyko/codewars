1b711effb160511b09cee06f3b39bf88f8d347e2
Sergey <loveykosergey@ukr.net>
2022-01-07 22:38:49 +0200

    feat: task №116 - Thinkful - Logic Drills: Traffic light

---
+++ task_116.js
@@ -0,0 +1,16 @@
+//  116. - Thinkful - Logic Drills: Traffic light
+
+function updateLight(current) {
+    switch(current) {
+        case "green":
+            return "yellow";
+        case "yellow":
+            return "red";
+        case "red":
+            return "green";
+    }
+}
+
+console.log(updateLight("green"));      //  "yellow"
+console.log(updateLight("yellow"));     //  "red"
+console.log(updateLight("red"));        //  "green"
