522dc6b81f48fa9001be48941094a65c1f4295e8
Sergey <loveykosergey@ukr.net>
2022-01-07 22:49:44 +0200

    feat: task №118 - Will you make it?

---
+++ task_118.js
@@ -0,0 +1,8 @@
+//  118. - Will you make it?
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+    return distanceToPump <= mpg * fuelLeft;
+};
+
+console.log(zeroFuel(50, 25, 2));       //  true
+console.log(zeroFuel(100, 50, 1));      //  false
