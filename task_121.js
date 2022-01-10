9dd54a0ef487a6135dfa615b481a7846a48bf351
Sergey <loveykosergey@ukr.net>
2022-01-10 20:13:16 +0200

    feat: task №121 - Is this my tail?

---
+++ task_121.js
@@ -0,0 +1,15 @@
+//  121. - Is this my tail?
+
+function correctTail(body, tail) {
+  
+    const sub = body.substr(body.length - (tail.length));
+    
+    if (sub === tail){
+      return true;
+    }
+    return false;
+}
+
+console.log(correctTail("Fox", "x"));       //  true
+console.log(correctTail("Rhino", "o"));     //  true
+console.log(correctTail("Meerkat", "t"));   //  true   
