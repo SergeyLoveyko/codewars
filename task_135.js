1c5a15826233a2bf298b383e39e3c03cfd81db73
Sergey <loveykosergey@ukr.net>
2022-01-24 21:33:50 +0200

    feat: task №135 - Vowel remover

---
+++ task_135.js
@@ -0,0 +1,10 @@
+//  135. - Vowel remover
+
+function shortcut (string) {
+    return string.replace(/[aeiou]/g, '');
+}
+
+console.log(shortcut ('hello'));                    //  'hll'
+console.log(shortcut ('how are you today?'));       //  'hw r y tdy?'
+console.log(shortcut ('complain'));                 //  'cmpln'
+console.log(shortcut ('never'));                    //  'nvr'
