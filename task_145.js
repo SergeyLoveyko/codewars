74a5f6b2098e26af5ad1eceeb40902c04d950d3f
Sergey <loveykosergey@ukr.net>
2022-02-03 23:23:05 +0200

    feat: task №145 - Opposites Attract

---
+++ task_145.js
@@ -0,0 +1,15 @@
+//  145. - Opposites Attract
+
+function lovefunc(flower1, flower2){
+
+    if ((flower1 % 2 === 0 && flower2 % 2 === 0) || (flower1 % 2 !== 0 && flower2 % 2 !== 0)   ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(lovefunc(1,4));     //  true
+console.log(lovefunc(2,2));     //  false
+console.log(lovefunc(0,1));     //  true
+console.log(lovefunc(0,0));     //  false
