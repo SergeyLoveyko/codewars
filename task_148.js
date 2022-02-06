01d195e1ce2d100898b6417ccdcd497707be6ca3
Sergey <loveykosergey@ukr.net>
2022-02-06 23:27:29 +0200

    feat: task №148 - DNA to RNA Conversion

---
+++ task_148.js
@@ -0,0 +1,9 @@
+//  148. - 
+
+function DNAtoRNA(dna) {
+    return dna.replace(/T/g, "U");
+}
+
+console.log(DNAtoRNA("TTTT"));          //  "UUUU"
+console.log(DNAtoRNA("GCAT"));          //  "GCAU"
+console.log(DNAtoRNA("GACCGCCGCC"));    //  "GACCGCCGCC"
