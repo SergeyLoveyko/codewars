03549d8d708e0f511980a50616e268e45bfbb232
Sergey <loveykosergey@ukr.net>
2022-02-07 22:25:45 +0200

    feat: task №149 - Convert number to reversed array of digits

---
+++ task_149.js
@@ -0,0 +1,8 @@
+//  149. - Convert number to reversed array of digits
+
+function digitize(n) {
+    return n.toString().split('').map(Number).reverse();
+}
+
+console.log(digitize(35231));      //  [1,3,2,5,3]
+console.log(digitize(0));          //  [0]
