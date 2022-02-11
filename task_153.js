4825f102eeb88c1d72af4d1d520b461d904b2a50
Sergey <loveykosergey@ukr.net>
2022-02-11 20:52:10 +0200

    feat: task №153 - Take an Arrow to the knee, Functionally

---
+++ task_153.js
@@ -0,0 +1,8 @@
+//  153. - Take an Arrow to the knee, Functionally
+
+var ArrowFunc = function(arr) {
+    return arr.map((index) => String.fromCharCode(index)).join(''); //Complete this function
+}
+
+console.log(ArrowFunc([84,101,115,116]));                       //  'Test', 'Convert those numbers to letters'
+console.log(ArrowFunc([70,85,83,32,82,79,72,32,68,65,72]));     //  'FUS ROH DAH', 'Keep it up!'
