3a01cee7631d465a1189737125be2c8147c61d5a
Sergey <loveykosergey@ukr.net>
2022-02-17 22:56:18 +0200

    feat: task №160 - Is he gonna survive?

---
+++ task_160.js
@@ -0,0 +1,12 @@
+//  160. - Is he gonna survive?
+
+function hero(bullets, dragons){
+    return dragons * 2 <= bullets ? true : false;
+}
+
+console.log(hero(10, 5));               //  true
+console.log(hero(7, 4));                //  false
+console.log(hero(4, 5));                //  false
+console.log(hero(100, 40));             //  true
+console.log(hero(1500, 751));           //  false
+console.log(hero(0, 1));                //  false
