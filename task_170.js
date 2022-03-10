68cb001f566d59473560f4c37b3525f22274fa89
Sergey <loveykosergey@ukr.net>
2022-03-10 23:04:43 +0200

    feat: task №170 - Enumerable Magic #25 - Take the First N Elements

---
+++ task_170.js
@@ -0,0 +1,7 @@
+//  170. - Enumerable Magic #25 - Take the First N Elements
+
+function take(arr, n) {
+    return arr.slice(0, n);
+}
+
+console.log(take([0, 1, 2, 3, 5, 8, 13], 3));     //  [0, 1, 2]
