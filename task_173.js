954ffd141f5766d6c77c082546bf33a7405bc5f4
Sergey <loveykosergey@ukr.net>
2022-03-18 23:28:27 +0200

    feat: task №173 - Hex to Decimal

---
+++ task_173.js
@@ -0,0 +1,11 @@
+// 173. - Hex to Decimal
+
+function hexToDec(hexString){
+    return parseInt(hexString, 16);
+}
+
+console.log(hexToDec("1"));             //  1
+console.log(hexToDec("a"));             //  10
+console.log(hexToDec("10"));            //  16
+console.log(hexToDec("FF"));            //  255
+console.log(hexToDec("-C"));            //  -12
