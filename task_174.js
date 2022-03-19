c5ff0496297ea7e5a4524fd7e3858a08f43952cb
Sergey <loveykosergey@ukr.net>
2022-03-19 23:12:06 +0200

    feat: task №174 - Convert a string to an array

---
+++ task_174.js
@@ -0,0 +1,9 @@
+//  174. - Convert a string to an array
+
+function stringToArray(string){
+    return string.split(' ');
+}
+
+console.log(stringToArray("Robin Singh"));          //  ["Robin", "Singh"]
+console.log(stringToArray("I love arrays they are my favorite"));
+//  ["I", "love", "arrays", "they", "are", "my", "favorite"]
